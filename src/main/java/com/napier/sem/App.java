d744ca6a9e91672375bb9b095177268720334db2
40683193 <40683193@napier.ac.uk>
2024-02-15 23:17:00 -0400

    Display Employee Information tested and working.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -19,57 +19,57 @@ public class App {
         a.disconnect();
     }
 
-        /**
-         * Connection to MySQL database.
-         */
-        private Connection con = null;
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
 
-        /**
-         * Connect to the MySQL database.
-         */
-        public void connect ()
-        {
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect ()
+    {
+        try {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i) {
+            System.out.println("Connecting to database...");
             try {
-                // Load Database driver
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            } catch (ClassNotFoundException e) {
-                System.out.println("Could not load SQL driver");
-                System.exit(-1);
-            }
-
-            int retries = 10;
-            for (int i = 0; i < retries; ++i) {
-                System.out.println("Connecting to database...");
-                try {
-                    // Wait a bit for db to start
-                    Thread.sleep(30000);
-                    // Connect to database
-                    con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
-                    System.out.println("Successfully connected");
-                    break;
-                } catch (SQLException sqle) {
-                    System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                    System.out.println(sqle.getMessage());
-                } catch (InterruptedException ie) {
-                    System.out.println("Thread interrupted? Should not happen.");
-                }
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            } catch (InterruptedException ie) {
+                System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
-        /**
-         * Disconnect from the MySQL database.
-         */
-        public void disconnect ()
-        {
-            if (con != null) {
-                try {
-                    // Close connection
-                    con.close();
-                } catch (Exception e) {
-                    System.out.println("Error closing connection to database");
-                }
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect ()
+    {
+        if (con != null) {
+            try {
+                // Close connection
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error closing connection to database");
             }
         }
+    }
     public Employee getEmployee(int ID)
     {
         try
@@ -118,4 +118,3 @@ public class App {
         }
     }
 }
-
